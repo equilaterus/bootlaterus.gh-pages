bf4bc822b0975556ce6f6eb2a9d80335a6365d27
dacanizares <dacanizares@outlook.com>
2020-03-08 21:44:29 -0500

    Installing script merged into a single file
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>

--- _utils/install-site.ps1
+++ _utils/install-site.ps1
@@ -1,7 +1,8 @@
 . $PSScriptRoot/_EquilaterusCLI.ps1
 
 Function Invoke-Script {
-    bundle install
+  gem install bundler
+  bundle install
 }
 
 Start-Cli -Title 'Install Site' -Filename 'Gemfile'
